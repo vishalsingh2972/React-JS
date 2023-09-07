85a7a91267142dd254af7a3eafabec7d4c121f1a
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-09-08 01:42:21 +0530

    2

--- reactjs-1/app/page.js
+++ reactjs-1/app/page.js
@@ -1,9 +1,20 @@
+//🚀 rafce (React arrow function component export)
+
 import React from 'react'
 
-const page = () => {
+const page = () => { //page variable is a function that returns a div element with the text "Bittu mast sikhlera React uff"
   return (
     <div>Bittu mast sikhlera React uff</div>
   )
+  //return <h1>hello BITTU</h1>
 }
 
-export default page
+export default page //exporting to Next, now page variable/function can be used in other parts of the Next.js application
+
+// function page2(){
+//      return <h1>hello BITTU</h1>
+//  }
+
+//export default page2
+
+//🚀 JSX (JavaScript XML)

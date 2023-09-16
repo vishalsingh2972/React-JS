621b4b98fbae4f709038a9b5c937f7fc780367fb
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-09-17 01:33:00 +0530

    done

--- reactjs-1/app/trial.js
+++
@@ -1,7 +0,0 @@
-const h1 = document.querySelector('h1');
-
-const updateButton = document.querySelector('button');
-
-updateButton.addEventListener('click', () => {
-  h1.textContent = 'My marks were 33';
-});

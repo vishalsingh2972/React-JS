6957e47a795d7c95650f774855c547e97c921457
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-11-27 21:52:48 +0530

    done

--- reactjs-1/app/page0.js
+++ reactjs-1/app/page0.js
@@ -52,21 +52,37 @@ import React, { useState } from 'react'
 // };
 // export default page5
 
-const page6 = () => { 
-  let marks = 76;
-  return (
-    //Fragments <> </> or empty tags used as wrapper or wrapping in React
-   <> 
-     <h1 className='font-bold text-2xl text-blue-600'>My total marks were {marks}</h1>
-     <button onClick={() => {
-      marks = 33;
-     }} className='bg-gray-400 px-5 py-4 rounded mt-5 ml-3 text-white font-bold'>Update</button>
-   </>
-  )
-};
-export default page6
+// const page6 = () => { 
+//   let marks = 76;
+//   return (
+//     //Fragments <> </> or empty tags used as wrapper or wrapping in React
+//    <> 
+//      <h1 className='font-bold text-2xl text-blue-600'>My total marks were {marks}</h1>
+//      <button onClick={() => {
+//       marks = 33;
+//      }} className='bg-gray-400 px-5 py-4 rounded mt-5 ml-3 text-white font-bold'>Update</button>
+//    </>
+//   )
+// };
+// export default page6
 
 // const page7 = () => { 
+//   let marks = 76;
+//   const changeMarks = () => {
+//     console.log(marks);
+//     marks = 'fail ho gaya';
+//     console.log(marks);
+//   };
+//   return (
+//    <> 
+//      <h1 className='font-bold text-2xl text-blue-600'>My total marks were {marks}</h1>
+//      <button onClick={changeMarks()} className='bg-gray-400 px-5 py-4 rounded mt-5 ml-3 text-white font-bold'>Update</button>
+//    </>
+//   )
+// };
+// export default page7
+
+// const page8 = () => { 
 //   let marks = 76;
 //   const changeMarks = () => {
 //     console.log(marks);
@@ -82,10 +98,10 @@ export default page6
 //    </>
 //   )
 // };
-// export default page7
+// export default page8
 
 //🚀 useState Hook
-const page8 = () => {
+const page9 = () => {
   let [marks, setMarks] = useState(80); //way of creating variables in React is by using useStateSnippet/ "useState" hook //abhi ke liye think useState = React ke variables
   return (
     <>
@@ -98,4 +114,4 @@ const page8 = () => {
     </>
   )
 };
-export default page8
+export default page9

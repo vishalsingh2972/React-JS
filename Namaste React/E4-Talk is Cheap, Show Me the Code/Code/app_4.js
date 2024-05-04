691aa7fb5a4dcbae47492cc17c5bcbf373ae1d73
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-05-05 02:16:29 +0530

    Update app_4.js

--- Namaste React/E4-Talk is Cheap, Show Me the Code/Code/app_4.js
+++ Namaste React/E4-Talk is Cheap, Show Me the Code/Code/app_4.js
@@ -1,4 +1,6 @@
 //our Food Ordering App - SWIGATO
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 /* App basic blueprint
 
@@ -16,5 +18,34 @@
   - Links
   - Address
   - Contact info
-  
+
 */
+
+const Header = () => {
+  return(
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="https://images.app.goo.gl/nipkGUM5q7bK99DQ7"/>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+const AppLayout = () => {
+  return(
+    <div className="app">
+      <Header/>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);

eb9c40e83bdbdacb1b21abe5fa689de72f1c7b72
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-05-04 20:56:31 +0530

    Update app_4.js

--- Namaste React/E4-Talk is Cheap, Show Me the Code/Code/app_4.js
+++ Namaste React/E4-Talk is Cheap, Show Me the Code/Code/app_4.js
@@ -1,1 +1,19 @@
+//our Food Ordering App - SWIGATO
 
+/* App basic blueprint
+
+1) Header
+  - Logo
+  - Nav Items (Home, About us,.... this card)
+
+2) Body
+  - Search bar
+  - Restaurant card container
+    -- Restaurant card 
+
+3) Footer
+  - Copyright disclaimer
+  - Links
+  - Address
+  - Contact info
+*/

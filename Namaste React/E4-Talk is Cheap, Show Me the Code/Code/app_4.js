892e830a17f39d1fb44c6523fb77518c90d88a10
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-05-17 20:16:12 +0530

    e4 notes added

--- Namaste React/E4-Talk is Cheap, Show Me the Code/Code/app_4.js
+++ Namaste React/E4-Talk is Cheap, Show Me the Code/Code/app_4.js
@@ -76,6 +76,10 @@ const Body = () => {
       <div className="search">Search</div>
       <div className="res-container">
         <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
       </div>
     </div>
   )

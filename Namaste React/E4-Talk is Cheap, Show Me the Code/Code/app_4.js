44bde05094e17c30127b84da5bc60fbb8a865805
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-05-26 01:25:54 +0530

    config driven UI demo working fine 🍇🥑  🍞  🍱  🦀  🍦 🍼  🥢

--- Namaste React/E4-Talk is Cheap, Show Me the Code/Code/app_4.js
+++ Namaste React/E4-Talk is Cheap, Show Me the Code/Code/app_4.js
@@ -55,8 +55,10 @@ const Header = () => {
 
 //Direct Attack using style={{backgroundColor: "#f0f0f0"}}
 const RestaurantCard = (props) => {
+  // const RestaurantCard = ({resName, cuisine}) => { //Another way where we are directly "destructuring on the fly" 
   console.log(props); //JS object
-  const{resName, cuisine} = props; //short for const resName = props.resName; and const cuisine = props.cuisine;
+  // const{resName, cuisine} = props; //short for const resName = props.resName; and const cuisine = props.cuisine;
+   const{ resData } = props;
   return(
     <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
       <img
@@ -64,12 +66,24 @@ const RestaurantCard = (props) => {
         alt="res_food_image"
         src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/xxg1pthsrrxpd83in6r3"
       />
-      <h3>{resName}</h3>
-      <h4>{cuisine}</h4>
-      <h4>⭐ 4.5</h4>
-      <h4>20-25 min</h4>
+      <h3>{resData.data.name}</h3>
+      <h4>{resData.data.cuisines}</h4>
+      <h4>⭐ {resData.data.avgRating}</h4>
+      <h4>₹ {(resData.data.costForTwo) / 100}</h4>
+      <h4>{resData.data.deliveryTime} minutes</h4>
     </div>
   )
+}
+
+const resObj = {
+  type: "restaurant",
+  data:{
+    name: "KFC",
+    cuisines: ["Animal","Bakra","Chicken"],
+    avgRating: "4",
+    costForTwo: "40000",
+    deliveryTime: "25"
+  }
 }
 
 //destructuring directly as "{resName, cuisine}" instead of using word "props"
@@ -90,15 +104,14 @@ const RestaurantCard = (props) => {
 // }
 
 //Props - Properties - same as we have arguments for functions, for components we pass data via props or by using props //Hence, passing a prop to a component is just like passing an argument to a function
-//as seen below - resName="PizzaHut" cuisine="Pizza" these are the props being passed to the component <RestaurantCard/>, so here react will wrap everything (i.e resName="PizzaHut" cuisine="Pizza") inside an object and send it as argument "props" in const RestaurantCard = (props)...
+//as seen below - resName="PizzaHut" cuisine="Pizza" these are the props being passed to the component <RestaurantCard/>, so here react will wrap everything (i.e resName="PizzaHut" cuisine="Pizza") inside an object and send it as argument "props" as seen above in const RestaurantCard = (props)...
 //hence whenever we want to dynamically pass in some data to a component, we pass it as a prop
 const Body = () => {
   return(
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard resName="Pista House" cuisine="Indian, Chinese, Italian"/> 
-        <RestaurantCard resName="PizzaHut" cuisine="Pizza"/>
+        <RestaurantCard resData={resObj}/>
       </div>
     </div>
   )

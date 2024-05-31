2384566282e697044d05b245ced4d86d31ee2cab
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-05-31 10:49:01 +0530

    Update Body.jsx

--- Namaste React/E5-Let's get Hooked/Code/src/components/Body.jsx
+++ Namaste React/E5-Let's get Hooked/Code/src/components/Body.jsx
@@ -2,13 +2,67 @@ import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
+
+  let listOfRestaurants = [
+    {
+    "info": {
+      "id": "595197",
+      "name": "Pizza Hut",
+      "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      "costForTwo": "₹350 for two",
+      "cuisines": [
+        "Pizzas","Samosa","Burger"
+      ],
+      "avgRating": 4.2,
+      "avgRatingString": "4.2",
+      "sla": {
+        "deliveryTime": 59,
+      }
+    }
+    },
+    {
+    "info": {
+      "id": "595196",
+      "name": "Dominos",
+      "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      "costForTwo": "₹350 for two",
+      "cuisines": [
+        "Pizzas","Samosa","Burger"
+      ],
+      "avgRating": 3.8,
+      "avgRatingString": "3.8",
+      "sla": {
+        "deliveryTime": 59,
+      }
+    }
+    },
+    {
+    "info": {
+      "id": "595198",
+      "name": "Subway",
+      "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      "costForTwo": "₹350 for two",
+      "cuisines": [
+        "Pizzas","Samosa","Burger"
+      ],
+      "avgRating": 4.1,
+      "avgRatingString": "4.1",
+      "sla": {
+        "deliveryTime": 59,
+      }
+    }
+    }
+];
+
   return(
     <div className="body">
       <div className="filter">
         <button 
         className="filter-btn" 
-        onMouseOver={() => {
-          console.log("Button Clicked");
+        onClick={() => {
+          //Filter logic here ⬇️
+          listOfRestaurants = listOfRestaurants.filter((res)=> res.info.avgRating > 4)
+          console.log(listOfRestaurants);
         }}
         >
           Top Rated Restaurants
@@ -16,7 +70,7 @@ const Body = () => {
       </div>
       <div className="res-container">
         {
-          resList.map((restaurant, index) => (
+          listOfRestaurants.map((restaurant, index) => (
             <RestaurantCard key={restaurant.info.id} resData={restaurant}/>        
           ))
         }

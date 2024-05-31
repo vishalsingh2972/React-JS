ca121eee4d26a1d4044d7ac96b8796aa90973b52
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-05-31 10:07:30 +0530

    Update Body.jsx

--- Namaste React/E5-Let's get Hooked/Code/src/components/Body.jsx
+++ Namaste React/E5-Let's get Hooked/Code/src/components/Body.jsx
@@ -4,7 +4,16 @@ import RestaurantCard from "./RestaurantCard";
 const Body = () => {
   return(
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button 
+        className="filter-btn" 
+        onClick={() => {
+          console.log("Button Clicked");
+        }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
         {
           resList.map((restaurant, index) => (

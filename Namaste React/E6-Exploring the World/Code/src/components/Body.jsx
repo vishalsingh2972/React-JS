c57f2e7e8d16b201fbc77d029f6980bacf47b8e0
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-06-08 16:07:57 +0530

    Swiggy API

--- Namaste React/E6-Exploring the World/Code/src/components/Body.jsx
+++ Namaste React/E6-Exploring the World/Code/src/components/Body.jsx
@@ -7,7 +7,14 @@ const Body = () => {
 
   useEffect(() => {
     console.log("useEffect called 3")
+    fetchData();
   }, []);
+
+  const fetchData = () => {
+    const data = fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+  }
 
   console.log("Body rendered 1");
   return(

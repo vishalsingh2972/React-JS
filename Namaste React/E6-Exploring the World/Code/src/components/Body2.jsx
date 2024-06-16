3d9a1cc5eab41d07d1558441d3777459e6191f9e
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-06-16 13:45:37 +0530

    all set

--- Namaste React/E6-Exploring the World/Code/src/components/Body2.jsx
+++ Namaste React/E6-Exploring the World/Code/src/components/Body2.jsx
@@ -2,18 +2,17 @@ import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 
-const Body = () => {
+const Body2 = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
-  //console.log({listOfRestaurants})
+
   const [filteredRestaurant, setFilteredRestaurant] = useState([]);
 
   const [searchText, setSearchText] = useState("");
-  // Whenever state variables get updated (here 'listOfRestaurants' and 'searchText' are state variables), react triggers a reconciliation cycle i.e. react re-renders the component again
+  
   console.log("full body component got rendered again");
 
 
   useEffect(() => {
-    // console.log("useEffect called 3")
     fetchData();
   }, []);
 
@@ -21,26 +20,13 @@ const Body = () => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
-    
-    //console.log(data);
-    
     const json = await data.json();
     console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
-
     setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    setFilteredRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
   }
 
-  // ⬇️ Conditional Rendering ⬇️
-  // if(listOfRestaurants.length === 0){
-  //   return <h1>Loading......please be patient 1 ❤️</h1>
-  //   return <Shimmer/>
-  // }
-
-  // console.log("Body rendered 1");
-  return( listOfRestaurants.length === 0 ? (<Shimmer/>) : (
+  return(
     <div className="body">
-      {/* {  console.log("Body rendered 2") } */}
       <div className="filter">
         <div className="search">
           <input 
@@ -50,16 +36,11 @@ const Body = () => {
             value={searchText} 
             onChange={(event) => {
               setSearchText(event.target.value);
-              // console.log(event.target.value)
             }}
           />
           <button 
             onClick={() => {
-              // Filter the restaurant cards and update the UI accordingly
-              // console.log(searchText);
-
               if (!searchText) {
-                // Display popup if searchText is empty
                 alert("Please enter a restaurant name!");
               }
 
@@ -68,14 +49,12 @@ const Body = () => {
               )
 
               filteredRestaurant.length === 0 ? alert("Restaurant not found") : setFilteredRestaurant(filteredRestaurant);
-              // filteredRestaurant.length === 0 ? (setFilteredRestaurant([]), alert("Restaurant not found")) : setFilteredRestaurant(filteredRestaurant);
             }}
           >Search</button>
         </div>     
         <button 
           className="filter-btn" 
           onClick={() => {
-            //Filter logic here ⬇️
             const filteredList = listOfRestaurants.filter((res)=> res.info.avgRating >= 4)
             setListOfRestaurants(filteredList);
           }}
@@ -84,14 +63,18 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {
+      {
+        filteredRestaurant && filteredRestaurant.length > 0 ? (
           filteredRestaurant.map((restaurant, index) => (
             <RestaurantCard key={restaurant.info.id} resData={restaurant}/>        
           ))
-        }
+        ) : (
+          <Shimmer/>
+        )
+      }
       </div>
     </div>
-  ))
+  )
 }
 
-export default Body
+export default Body2

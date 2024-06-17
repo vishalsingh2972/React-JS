3c3d0140df5eb1273d922a777564a70779fcf89e
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-06-17 15:17:43 +0530

    Update Body.jsx

--- Namaste React/E6-Exploring the World/Code/src/components/Body.jsx
+++ Namaste React/E6-Exploring the World/Code/src/components/Body.jsx
@@ -21,6 +21,11 @@ const Body = () => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
+
+    //use in case of trying to bypass cors without using cors chrome plugin
+    // const data = await fetch(
+    //   "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    // );
     
     //console.log(data);
     

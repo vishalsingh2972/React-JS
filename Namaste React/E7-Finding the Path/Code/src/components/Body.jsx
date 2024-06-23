2a644330ba98e65ba48b47c1a0341b9dfbe73c96
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-06-23 13:52:57 +0530

    set

--- Namaste React/E7-Finding the Path/Code/src/components/Body.jsx
+++ Namaste React/E7-Finding the Path/Code/src/components/Body.jsx
@@ -11,37 +11,25 @@ const Body = () => {
   // Whenever state variables get updated (here 'listOfRestaurants' and 'searchText' are state variables), react triggers a reconciliation cycle i.e. react re-renders the component again
   console.log("full body component got rendered again");
 
-
+  //if dependency array is empty = [] => useEffect is called on initial render(just once)
   useEffect(() => {
     // console.log("useEffect called 3")
     fetchData();
-  }, []);
+  }, []); //empty dependency array [], hence useEffect hook will get triggered (i.e. "console.log("useEffect called 3")" and "fetchData();" will happen) just only once when my Body component is rendered for the first time
 
   const fetchData = async () => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
-
-    //use in case of trying to bypass cors without using cors chrome plugin
-    // const data = await fetch(
-    //   "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    // );
-    
-    //console.log(data);
     
     const json = await data.json();
-    console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    // console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
 
     setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     setFilteredRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
   }
 
   // ⬇️ Conditional Rendering ⬇️
-  // if(listOfRestaurants.length === 0){
-  //   return <h1>Loading......please be patient 1 ❤️</h1>
-  //   return <Shimmer/>
-  // }
-
   // console.log("Body rendered 1");
   return( listOfRestaurants.length === 0 ? (<Shimmer/>) : (
     <div className="body">
--- Namaste React/E7-Finding the Path/Code/src/components/Header.jsx
+++ Namaste React/E7-Finding the Path/Code/src/components/Header.jsx
@@ -1,11 +1,27 @@
 import { LOGO_URL } from "../utils/constants"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Header = () => {
 
   let btn = "Login 0";
   const [btn2, setButton] = useState("Login");
   console.log("full header component got rendered again");
+
+  // 3 cases of useEffect
+  //1) no dependency array [] => hence useEffect is called on every component render
+  //2) if dependency array is empty = [] => useEffect is called on initial render(just once)
+  //3) if we put somthing inside the dependency array = [btn2] => then useEffect will omly be called when the dependency changes i.e. here useEffect is called everytime btn2 is updated
+  useEffect(() => {
+    // console.log("useEffect called 1")
+  }); //no dependency array [], hence useEffect hook will get triggered each time my Header component is rendered on the UI
+
+  useEffect(() => {
+    // console.log("useEffect called 2")
+  }, []); //empty dependency array [], hence useEffect hook will get triggered just once only when my Header component is rendered for the first time
+
+  useEffect(() => {
+    console.log("useEffect called 3")
+  }, [btn2]);
 
   return(
     <div className="header">

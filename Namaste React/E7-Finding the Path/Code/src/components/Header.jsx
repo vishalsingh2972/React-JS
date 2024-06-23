6a52e2bf3a9400f771fd879fad3f589ea89cc990
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-06-23 20:56:56 +0530

    Link component

--- Namaste React/E7-Finding the Path/Code/src/components/Header.jsx
+++ Namaste React/E7-Finding the Path/Code/src/components/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
       <div className="nav-items">
         <ul>
           <li>Home</li>
-          <li><a href="/about">About Us</a></li>
+          <li><Link to="/about">About Us</Link></li>
           <li><Link to="/contact">Contact Us</Link></li>
           <li>Cart</li>
           <button className="login-button" onClick={() => {

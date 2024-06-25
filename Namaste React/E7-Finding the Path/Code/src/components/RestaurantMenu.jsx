4204334ffbb6868911d45084eeb3e03609e9ac36
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-06-25 13:53:09 +0530

    error at "const data = await fetch(MENU_API + {resId})" need to check this

--- Namaste React/E7-Finding the Path/Code/src/components/RestaurantMenu.jsx
+++ Namaste React/E7-Finding the Path/Code/src/components/RestaurantMenu.jsx
@@ -1,33 +1,53 @@
 import { useEffect, useState } from "react"
 import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+import { MENU_API } from "../utils/constants";
 
 const RestaurantMenu = () => {
+
+  const [restaurantInfo, setRestaurantInfo] = useState(null);
+ 
+  const {resId} = useParams(); //or simply const resId = useParams();
+  console.log(resId);
 
   useEffect(() => {
     fetchMenu();
   }, []); //giving empty dependency array [] here as when we click the restaurant crad we want it to render only once when clicked
   
   const fetchMenu = async () => {
-    const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.0759837&lng=72.8776559&restaurantId=414109&catalog_qa=undefined&submitAction=ENTER")
+    const data = await fetch(MENU_API + {resId})
   
     const json = await data.json();
-    console.log(json);
+    // console.log(json);
+    // console.log(json.data);
+    setRestaurantInfo(json.data)
   }
 
-  const [restaurantInfo, setRestaurantInfo] = useState(null);
+  // const name = restaurantInfo?.cards[2]?.card?.card?.info.name;
+  // const cuisines = restaurantInfo?.cards[2]?.card?.card?.info.cuisines;
+  // const costForTwoMessage = restaurantInfo?.cards[2]?.card?.card?.info.costForTwoMessage;
 
-  if(restaurantInfo === null) {
-    return <Shimmer/>
-  }
+  // const { name, cuisines, costForTwoMessage } = restaurantInfo?.cards[2]?.card?.card?.info; //❌ not working need to check //const name = restaurantInfo?.cards[2]?.card?.card?.info.name
+  const { name, cuisines, costForTwoMessage, totalRatingsString, avgRatingString, avgRating } = restaurantInfo?.cards[2]?.card?.card?.info || {};
 
-  return (
+  // const {itemCards} = restaurantInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card; //❌ not working need to check
+  const itemCards = restaurantInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card.itemCards;
+  console.log(itemCards);
+
+  return (restaurantInfo === null) ? <Shimmer/> : (
     <div className="menu">
-      <h1>Name of the Restaurant</h1>
+      <h1>{name}</h1>
+      <h2 style={{ display: 'inline-block'}}>{avgRatingString} ({totalRatingsString})</h2> 
+      <h2 className='cost'>{costForTwoMessage}</h2>
+      <h3>{cuisines.join(', ')}</h3>
       <h2>Menu</h2>
       <ul>
-        <li>Biryani</li>
-        <li>Masala Dosa</li>
-        <li>Payasam</li>
+        {itemCards.map((item) => (
+          <li key={item.card.info.id}>
+            {item.card.info.name} - ₹{item.card.info.price/100 || item.card.info.defaultPrice/100}
+          </li>
+          // console.log(item.card.info.name)
+        ))}
       </ul>
     </div>
   )

dbb1b4d04f488fb8576be8fb9fd915b7272ab331
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-07-11 18:04:26 +0530

    Class Based Components intro done

--- Namaste React/E8-Let's get Classy/Code/src/components/About.jsx
+++ Namaste React/E8-Let's get Classy/Code/src/components/About.jsx
@@ -1,4 +1,5 @@
 import User from "./User"
+import UserClass from "./UserClass"
 
 const About = () => {
   return (
@@ -6,6 +7,8 @@ const About = () => {
       <h1>About</h1>
       <h2>This is your about section</h2>
       <User/>
+      <UserClass/>
+      
       {console.log("we are now in the About section")}
     </div>
   )
--- Namaste React/E8-Let's get Classy/Code/src/components/UserClass.jsx
+++ Namaste React/E8-Let's get Classy/Code/src/components/UserClass.jsx
@@ -1,1 +1,20 @@
+import React from "react"; //'React.Component' is a class inside React, hence importing React
 
+// Class based component: similar to functional based components, class based components are also simply a normal javascript class
+//(reminder: functional based components are simple normal javascript functions that return some jsx)
+
+class UserClass extends React.Component{ //'extends React.Component' will help React know/identify that this is a class based component and so React starts tracking it
+                                         // 'React.Component' is a class given to us by React and here 'UserClass' class is inheriting some properties from 'React.Component' class
+
+  //this render method will return some piece of jsx and that jsx eventually gets rendered on the screen
+  //so in short similar to functional based components, Class based component is a class which has a render method which returns some piece of jsx
+  render(){
+    return <div className="user-card">
+      <h2>Name: Vishal_Class</h2>
+      <h3>Location: Hyderabad_Class</h3>
+      <h4>Contact: @vishalsingh2972_Class</h4>
+    </div>
+  }
+}
+
+export default UserClass

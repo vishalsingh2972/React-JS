e0db849104ab450b6dd941466d03f90dd9d396eb
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-07-11 15:25:17 +0530

    Create User.jsx

---
+++ Namaste React/E8-Let's get Classy/Code/src/components/User.jsx
@@ -0,0 +1,9 @@
+const User = () => {
+  return <div className="user-card">
+    <h2>Name: Vishal</h2>
+    <h3>Location: Hyderabad</h3>
+    <h4>Contact: @vishalsingh2972</h4>
+  </div>
+}
+
+export default User

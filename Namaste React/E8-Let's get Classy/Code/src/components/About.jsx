255d6a174e744526978391f831041a6b41592de2
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-07-11 15:25:36 +0530

    Update About.jsx

--- Namaste React/E8-Let's get Classy/Code/src/components/About.jsx
+++ Namaste React/E8-Let's get Classy/Code/src/components/About.jsx
@@ -1,9 +1,11 @@
+import User from "./User"
+
 const About = () => {
   return (
     <div>
       <h1>About</h1>
       <h2>This is your about section</h2>
-      
+      <User/>
       {console.log("we are now in the About section")}
     </div>
   )

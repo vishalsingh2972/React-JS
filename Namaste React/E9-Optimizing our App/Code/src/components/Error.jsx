1f511961b9ac7c0658af17160c1f645804940db1
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-07-21 14:32:27 +0530

    Optimizing our App start

---
+++ Namaste React/E9-Optimizing our App/Code/src/components/Error.jsx
@@ -0,0 +1,19 @@
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+  
+  //useRouteError hook from react-router-dom helps you get more detailed information about the error that occurred during routing. This information can be used to display the error in a user-friendly manner on the UI.
+  const err = useRouteError(); //error element 'err'
+  console.log(err);
+  
+  return (
+    <div>
+      <h1>Oops !!!</h1>
+      <h2>Something went wrong</h2>
+
+      <h2>{err.status}: {err.statusText} - {err.error.message}</h2>
+    </div>
+  )
+}
+
+export default Error
---
+++ Namaste React/E9-Optimizing our App/Code/src/utils/constants.jsx
@@ -0,0 +1,5 @@
+export const LOGO_URL = "https://cdn.iconscout.com/icon/free/png-256/free-logo-1889525-1597585.png?f=webp";
+
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.0759837&lng=72.8776559&restaurantId=";

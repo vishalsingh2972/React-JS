823d591f283e504682c8fa60cfbc275077965d14
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-07-22 18:38:51 +0530

    Update Body.jsx

--- Namaste React/E5-Let's get Hooked/Code/src/components/Body.jsx
+++ Namaste React/E5-Let's get Hooked/Code/src/components/Body.jsx
@@ -6,7 +6,7 @@ import resList from "../utils/mockData";
 const Body = () => {
   //Super Powerful React variable - also known as State Variable, and for creating this State Variable we make use of React hooks, to be precise here we use useState hook
   //in simple words just like a functional component is simply a javascript function that returns a JSX and a react element is simply a javascript object similarly a React hook is also a javascript function which is given to us by React (its a prebuilt), the only difference being this javascript function comes with certain extra superpowers, this javascript function has some logic written BTS inside React, this javascript function is a utility function readily available to us by React
-  //in simple words React hook is a utility function provided to us by React
+  //in simple words React hook is a utility function provided to us by React that make work of our component more easier or that streamlines the work of your React components
   const [listOfRestaurants, setListOfRestaurants] = useState(resList); //Local State Variable -  scope inside the component it is targeting
   // console.log(useState(resList)); //array
 

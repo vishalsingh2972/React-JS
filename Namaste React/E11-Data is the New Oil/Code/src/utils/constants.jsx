7036548ef6921e13654393fd25b57a802a976c83
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-07-29 13:33:40 +0530

    Update constants.jsx

--- Namaste React/E11-Data is the New Oil/Code/src/utils/constants.jsx
+++ Namaste React/E11-Data is the New Oil/Code/src/utils/constants.jsx
@@ -3,3 +3,6 @@ export const LOGO_URL = "https://cdn.iconscout.com/icon/free/png-256/free-logo-1
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.0759837&lng=72.8776559&restaurantId=";
+
+//EXTRA
+export const RESTAURANTS_API = 'https://cors-handlers.vercel.app/api/?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D23.3773949%26lng%3D85.3580618%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING';

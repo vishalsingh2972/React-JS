c4e00600cd61c90d19d20153c977605e17c5f883
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-07-29 13:34:31 +0530

    Update Body.jsx

--- Namaste React/E11-Data is the New Oil/Code/src/components/Body.jsx
+++ Namaste React/E11-Data is the New Oil/Code/src/components/Body.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
+import { RESTAURANTS_API } from "../utils/constants";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
@@ -22,6 +23,7 @@ const Body = () => {
   const fetchData = async () => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      //RESTAURANTS_API
     );
     
     const json = await data.json();

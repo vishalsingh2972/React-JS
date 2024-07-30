1537dcd63c08e460664d71caebeb6ea459233bf2
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-07-30 23:19:17 +0530

    CORS Handler restaurant api added (EXTRA)

--- Namaste React/E11-Data is the New Oil/Code/src/utils/constants.jsx
+++ Namaste React/E11-Data is the New Oil/Code/src/utils/constants.jsx
@@ -6,5 +6,5 @@ export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_M
 
 export const RESTAURANTS_API = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
 
-// EXTRA
+//EXTRA
 export const RESTAURANTS_API2 = 'https://cors-handlers.vercel.app/api/?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D23.3773949%26lng%3D85.3580618%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING';

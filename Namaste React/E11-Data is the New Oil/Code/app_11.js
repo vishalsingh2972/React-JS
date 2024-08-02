8c1827a429f9bae787e5fb70c53175bd96ed12ad
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-08-03 01:28:52 +0530

    UserContext.Provider done

--- Namaste React/E11-Data is the New Oil/Code/app_11.js
+++ Namaste React/E11-Data is the New Oil/Code/app_11.js
@@ -1,5 +1,5 @@
 //our Food Ordering App - SWIGATO
-import React, { lazy, Suspense } from "react"; //lazy() function comes from "react" library present in node modules
+import React, { lazy, Suspense, useContext, useEffect, useState } from "react"; //lazy() function comes from "react" library present in node modules
 import ReactDOM from "react-dom/client";
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
@@ -9,6 +9,7 @@ import Contact from "./src/components/Contact";
 import Error from "./src/components/Error";
 import RestaurantMenu from "./src/components/RestaurantMenu";
 import Loading from "./src/components/Loading";
+import UserContext from "./src/utils/context/UserContext";
 
 // import Grocery from "./src/components/GroceryApp/Grocery"; //importing Grocery in normal way where Grocery also gets included together with Food App bundle
 
@@ -22,12 +23,31 @@ import Loading from "./src/components/Loading";
 const Grocery2 = lazy(() => import("./src/components/GroceryApp/Grocery")); //importing Grocery in a 'LAZY' way where Grocery will be sent to the browser as a separate bundle (and not gets included together with the Food App bundle)
 
 const AppLayout = () => {
-  return(
-    <div className="app">
-      <Header/>
 
-      <Outlet/>
-    </div>
+  const { loggedInUser } = useContext(UserContext);
+  const [userName, setUserName] = useState(loggedInUser); //set initial value as loggedInUser as given in UserContext.jsx
+
+  useEffect(() => {
+    //Make API call do authentication if username password verify go below
+
+    //afer autentication passed
+    setTimeout(() => {
+      const data = {
+        name: "Sinamika"
+      }
+      setUserName(data.name);
+    }, 1000);
+  }, [])
+
+  return (
+    <UserContext.Provider value={{ loggedInUser: userName }}>
+      <div className="app">
+        {/* <UserContext.Provider value={{loggedInUser: 'Elon Musk'}}>*/}   {/* wrapping only header case: changes made in UserContext.Provider will only get reflected in Header, can also give different value here than the above UserContext.Provider */}
+        <Header />
+        {/* </UserContext.Provider> */}
+        <Outlet />
+      </div>
+    </UserContext.Provider>
   )
 }
 
@@ -35,35 +55,35 @@ const AppLayout = () => {
 const appRouter = createBrowserRouter([
   {
     // path: "/", (need to check this - once try without putting this) <AppLayout /> component will be rendered automatically for the root path (/) without explicitly specifying the path in the route configuration.
-    element: <AppLayout/>, //basically if my path is "/" then load my entire Homepage or AppLayout
+    element: <AppLayout />, //basically if my path is "/" then load my entire Homepage or AppLayout
     children: [
       {
         path: "/",
-        element: <Body/>
+        element: <Body />
       },
       {
         path: "/about",
-        element: <About/>,
+        element: <About />,
         // errorElement: <Error/> //redundant errorElement - can be removed - Root-level error handling will manage this
       },
       {
         path: "/contact",
-        element: <Contact/>,
+        element: <Contact />,
         // errorElement: <Error/> //redundant errorElement - can be removed - Root-level error handling will manage this
       },
       {
         path: "/grocery",
         // element: <Grocery/> //normal loading when GroceryApp in same bundle
-        element: <Suspense fallback={<Loading/>}><Grocery2/></Suspense>, //lazy loading when GroceryApp is sent in a separate bundle
+        element: <Suspense fallback={<Loading />}><Grocery2 /></Suspense>, //lazy loading when GroceryApp is sent in a separate bundle
       },
       {
         path: "/restaurants/:resId", //":resId" will help us give a unique path to each restaurant, i.e. in this "/restaurants/:resId", ":resId" this part of the path is dynamic
-        element: <RestaurantMenu/>
+        element: <RestaurantMenu />
       }
     ],
-    errorElement: <Error/> //Root-level error handling
+    errorElement: <Error /> //Root-level error handling
   },
 ])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={appRouter}/>);
+root.render(<RouterProvider router={appRouter} />);

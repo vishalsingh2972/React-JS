7e9cb6396922a044f5f874b048e17a53828f9bf6
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-08-28 01:30:21 +0530

    RestaurantCard testing start

---
+++ Namaste React/E13-Time for Test/Code/src/__tests__/restaurantCard.test.jsx
@@ -0,0 +1,7 @@
+import { render } from "@testing-library/react"
+import RestaurantCard from "../components/RestaurantCard";
+
+it("should render RestaurantCard component with props Data that we pass in", () => {
+
+  render(<RestaurantCard/>)
+})

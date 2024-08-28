2f247aaa504b41c645e65c1f2fcf43070b0746b4
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-08-28 21:19:31 +0530

    search bar IT

---
+++ Namaste React/E13-Time for Test/Code/src/__tests__/integration_Tests/search.test.jsx
@@ -0,0 +1,7 @@
+import { render } from "@testing-library/react"
+import Body from "../../components/Body"
+
+it("should render the Body component with Search Button", () => {
+
+  render(<Body/>);
+})

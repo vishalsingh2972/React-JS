f585a253cb3444300e47bd9bbbaff721a7b3145d
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-09-08 12:14:16 +0530

    😵 need to check again

--- Namaste React/E13-Time for Test/Code/src/__tests__/integration_Tests/cart.test.jsx
+++ Namaste React/E13-Time for Test/Code/src/__tests__/integration_Tests/cart.test.jsx
@@ -1,6 +1,7 @@
 import { act, render, screen } from "@testing-library/react";
 import RestaurantMenu from "../../components/RestaurantMenu";
 import MOCK_DATA_IT_2 from "../mocks/IT/mockResMenu.json";
+// import { MemoryRouter } from 'react-router-dom';
 
 global.fetch = jest.fn(() => Promise.resolve
   ({
@@ -9,10 +10,12 @@ global.fetch = jest.fn(() => Promise.resolve
 );
 
 it("should load the Restaurant Menu component", async () => {
+  await act(async () => render(
+    // <MemoryRouter initialEntries={[`/restaurants/${MOCK_DATA_IT_2.data.cards[2].card.card.info.id}`]}>
+      <RestaurantMenu />
+    // </MemoryRouter>
+  ))
 
-  const chomu = await act(async () => render(<RestaurantMenu />));
-  console.log(chomu);
-
-  // const accordion_Header = screen.getByText("Ganpati Collection"); //😵 need to check again
+  const accordion_Header = screen.getByText("Ganpati Collection"); //😵 need to check again
 
 })
